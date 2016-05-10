b9d755e51713c8ea5a89bd9f8f550fcb5a21b819
ChrisVent <venturachristopher98@gmail.com>
2016-05-10 13:13:15 -0400

    sync functions in mkdir

--- bin/flicker.js
+++ bin/flicker.js
@@ -49,6 +49,10 @@ var app_pkg = {
     pug: "latest"
   }
 };
+
+ var time = 5;
+
+
 /* views */
 var errpug = loadFile('views/err.pug');
 var indexpug = loadFile('views/index.pug');
@@ -62,27 +66,47 @@ var indexjs = loadFile('public/js/index.js');
 /* public/css */
 var stylecss = loadFile('public/css/style.css');
 
-mkdir(app_dir + 'public');
-mkdir(app_dir + 'public/js');
-mkdir(app_dir + 'public/img');
-mkdir(app_dir + 'public/css');
-mkdir(app_dir + 'routers');
-mkdir(app_dir + 'views',() => {
+mkdir(app_dir + 'public', () => {
     Write(app_dir + 'app.js',appjs);
     Write(app_dir + 'package.json',JSON.stringify(app_pkg,null,2));
-    /* views */
+    info();
+});
+mkdir(app_dir + 'public/js', () => {
+    Write(app_dir + 'public/js/index.js',indexjs);
+    info();
+});
+mkdir(app_dir + 'public/img', () => {
+    info();
+});
+mkdir(app_dir + 'public/css', () => {
+    Write(app_dir + 'public/css/style.css',stylecss);
+    info();
+});
+mkdir(app_dir + 'routers', () => {
+    Write(app_dir + 'routers/home.js',homerouter);
+    Write(app_dir + 'routers/users.js',userrouter);
+    info();
+});
+mkdir(app_dir + 'views',() => {
     Write(app_dir + 'views/err.pug',errpug);
     Write(app_dir + 'views/index.pug',indexpug);
     Write(app_dir + 'views/layout.pug',layoutpug);
-    /* routers */
-    Write(app_dir + 'routers/home.js',homerouter);
-    Write(app_dir + 'routers/users.js',userrouter);
-    /* public */
-    /* public/js */
-    Write(app_dir + 'public/js/index.js',indexjs);
-    /* public/css */
-    Write(app_dir + 'public/css/style.css',stylecss);
+    info();
 });
+
+
+function info(){
+    if(time--){ return };
+    console.log('\n\n  \x1b[36minstall dependencies:\x1b[0m');
+    if(app_dir != ""){
+        console.log('\t$ cd ' + app_dir + ' && npm install');
+    }
+    else{
+        console.log('\t$ npm install');
+    }
+    console.log('  \x1b[36mrun the app:\x1b[0m');
+     console.log('\t$ npm start\n\n');
+};
 
 function Write(path,str){
     fs.writeFileSync(path,str,{mode: 0666 });
